1a61e4a3111f0be38debb31694a8d8e950196805
willtechcod <wills@willtech>
2021-07-21 04:04:25 -0300

    atualizando

---
+++ models/Usuario.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const Usuario = db.define('usuarios', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+
+});
+
+Usuario.sync();
+
+module.exports = Usuario;
